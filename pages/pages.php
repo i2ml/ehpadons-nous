4591b4e6bd0e8c8d2b9c6fe0dc671adeb1002ac3
BastLast <bastien.last87@gmail.com>
2021-05-03 16:23:33 +0200

    v1 menu

--- pages/pages.php
+++ pages/pages.php
@@ -1,5 +1,3 @@
-
-
 <?
 
 /**
@@ -11,48 +9,71 @@ get_header('compiled');
 ?>
 <div class="page-container page__pages">
 
-  <? get_view('page-intro') ?>
+    <? get_view('page-intro') ?>
 
-  <div class="inner">
-    
-      <?
-      $editions = get_posts([
-        'post_type'     => 'edition',
-        'posts_per_page' => -1,
-        'orderby'			  => 'menu_order',
-        'order'				  => 'ASC'
-      ]);
-      foreach($editions as $edition) {
-      ?>
-      <div class="page__pages-listTest">
-        <p>
-        <?php
-          echo $edition->annee;
-          ?>
-          </p>
-          <?php
-          foreach(get_field('pages') as $item) {
-          $page = $item['page'];
-          ?>
-          <div class="page__pages-item">
-            <h2><?= $page->post_title ?></h2>
-            <main>
-              <?= $item['description'] ?>
-            </main>
-            <a class="button-<?= $item['button_color'] ?>" href="<?= get_permalink($page->ID) ?>">
-              <?= $item['button_text'] ?>
-            </a>
+    <div class="inner">
+        <?
+        $editions = get_posts([
+            'post_type' => 'edition',
+            'posts_per_page' => -1,
+            'orderby' => 'title',
+            'order' => 'ASC'
+        ]);
+        $souseditions = get_posts([
+            'post_type' => 'sousedition',
+            'posts_per_page' => -1,
+            'orderby' => 'title',
+            'order' => 'ASC',
+        ]);
+        foreach ($editions as $edition) {
+            ?>
+            <div class="page__pages-listTest">
+                <p>
+                    <?php
+                    echo $edition->annee;
+                    ?>
+                </p>
+                <?php
+                foreach ($souseditions as $sousedition) {
+                    if (get_fields($sousedition->ID)['edition'] == $edition) {
+                        $editionfields = get_fields($sousedition->ID);
+                        ?>
+                        <div class="page__pages-item">
+                            <h2><?= $editionfields['region'] ?></h2>
+                            <main>
+                                <?= $editionfields['description'] ?>
+                            </main>
+                            <a class="button-<?= ["green","yellow","pink"][rand ( 0,2)] ?>" href=".">
+                                 VOIR LA PAGE
+                            </a>
 
-          </div>
-          <?php
-          echo get_permalink($page->ID);
-        }?>
-      </div>
-      <?php
-    }
-      ?>
-    
-  </div>
+                        </div>
+                        <?php
+                    }
+                }
+                //$page = $item['page'];
+                /*
+
+                ?>
+                <div class="page__pages-item">
+                    <h2><?= $page->post_title ?></h2>
+                    <main>
+                        <?= $item['description'] ?>
+                    </main>
+                    <a class="button-<?= $item['button_color'] ?>" href="<?= get_permalink($page->ID) ?>">
+                        <?= $item['button_text'] ?>
+                    </a>
+
+                </div>
+                <?php
+                echo get_permalink($page->ID);
+            } */ ?>
+            </div>
+            <?php
+        }
+        ?>
+
+    </div>
 
 </div>
 <?
