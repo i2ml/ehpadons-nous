2c797575aa3f6fdf16de403bfc3323dc62f297bc
BastLast <bastien.last87@gmail.com>
2021-05-04 14:15:53 +0200

    indentation fix

--- pages/pages.php
+++ pages/pages.php
@@ -11,9 +11,9 @@ get_header('compiled');
 
     <? get_view('page-intro') ?>
 
-        <div class="inner">
+    <div class="inner">
         <?
-        
+
         $editions = get_posts([
             'post_type' => 'edition',
             'posts_per_page' => -1,
@@ -29,60 +29,60 @@ get_header('compiled');
         foreach ($editions as $edition) {
             $compteur = 0;
             ?>
-                <div class="page__pages-Titre">
+            <div class="page__pages-Titre">
                 <p>
                     <?php
                     echo $edition->annee;
                     ?>
                 </p>
-                </div>
-                <div class="page__pages-list">
-                <?php
-                foreach ($souseditions as $sousedition) {
+            </div>
+            <div class="page__pages-list">
+            <?php
+            foreach ($souseditions as $sousedition) {
                 if (get_fields($sousedition->ID)['edition'] == $edition) {
-                        
-                                $editionfields = get_fields($sousedition->ID);
-                                ?>
-                                <div class="page__pages-item">
-                                    <h2><?= $editionfields['region'] ?></h2>
-                                    <main>
-                                        <?= $editionfields['description'] ?>
-                                    </main>
-                                    <a class="button-<?= ["green","yellow","pink"][rand ( 0,2)] ?>" href=".">
-                                        VOIR LA PAGE
-                                    </a>
-        
-                                </div>
-                                <?php
-                            if($compteur < 2){
-                            $compteur += 1;
-                        }else{?>
-                            </div>
-                            <div class="page__pages-list">
-                            <?php
-                            $compteur= 0;
-                        }
-                }?>
-                    
+
+                    $editionfields = get_fields($sousedition->ID);
+                    ?>
+                    <div class="page__pages-item">
+                        <h2><?= $editionfields['region'] ?></h2>
+                        <main>
+                            <?= $editionfields['description'] ?>
+                        </main>
+                        <a class="button-<?= ["green", "yellow", "pink"][rand(0, 2)] ?>" href=".">
+                            VOIR LA PAGE
+                        </a>
+                    </div>
                     <?php
-                }
-                //$page = $item['page'];
-                /*
+                    if ($compteur < 2) {
+                        $compteur += 1;
+                    } else {
+                        ?>
+                        </div>
+                        <div class="page__pages-list">
+                        <?php
+                        $compteur = 0;
+                    }
+                } ?>
 
-                ?>
-                <div class="page__pages-item">
-                    <h2><?= $page->post_title ?></h2>
-                    <main>
-                        <?= $item['description'] ?>
-                    </main>
-                    <a class="button-<?= $item['button_color'] ?>" href="<?= get_permalink($page->ID) ?>">
-                        <?= $item['button_text'] ?>
-                    </a>
-
-                </div>
                 <?php
-                echo get_permalink($page->ID);
-            } */ ?>
+            }
+            //$page = $item['page'];
+            /*
+
+            ?>
+            <div class="page__pages-item">
+                <h2><?= $page->post_title ?></h2>
+                <main>
+                    <?= $item['description'] ?>
+                </main>
+                <a class="button-<?= $item['button_color'] ?>" href="<?= get_permalink($page->ID) ?>">
+                    <?= $item['button_text'] ?>
+                </a>
+
+            </div>
+            <?php
+            echo get_permalink($page->ID);
+        } */ ?>
             </div>
             <?php
         }
